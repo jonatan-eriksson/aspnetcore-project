b58a9ff10e496ee0542e27ee62e18a478bd55430
alius <alius@LAPTOP-NNR66SG1>
2021-03-31 21:04:08 +0200

    add login

--- aspnetcore_project/Data/EventDbContext.cs
+++ aspnetcore_project/Data/EventDbContext.cs
@@ -17,30 +17,14 @@ namespace aspnetcore_project.Data
         }
 
         public DbSet<Event> Events { get; set; }
-        public DbSet<Organizer> Organizers { get; set; }
-        public DbSet<Attendee> Attendees { get; set; }
+      
 
         public void ResetAndSeed()
         {
             Database.EnsureDeleted();
             Database.EnsureCreated();
 
-            Attendee[] attendees = new Attendee[] {
-                new Attendee()
-                {
-                    Name = "Ian Holmes",
-                    PhoneNumber = "+1 203 42 564",
-                    Email = "ian.holmes@gmail.com",
-                },
-            };
-
-            Organizer[] organizers = new Organizer[] {
-                new Organizer(){
-                    Name = "Funcorp",
-                    Email = "info@funcorp.com",
-                    PhoneNumber = "+1 203 43 234",
-                },
-            };
+         
 
             Event[] events = new Event[] {
                 new Event(){
@@ -50,7 +34,7 @@ namespace aspnetcore_project.Data
                     Address="515 S Cascade Ave Colorado Springs, CO 80903",
                     Date=DateTime.Now.AddDays(34),
                     SpotsAvailable=234,
-                    Organizer= organizers[0],
+                   
                 },
                 new Event(){
                     Title="Moonhaven",
@@ -59,12 +43,11 @@ namespace aspnetcore_project.Data
                     Address="510 N McPherson Church Rd Fayetteville, NC 28303",
                     Date=DateTime.Now.AddDays(12),
                     SpotsAvailable=23,
-                    Organizer= organizers[0],
+                   
                 },
             };
 
-            AddRange(attendees);
-            AddRange(organizers);
+            
             AddRange(events);
 
             SaveChanges();
