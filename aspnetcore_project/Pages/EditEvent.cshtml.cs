b1dd32e3963b06162ca50c652006350c2da50ff8
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-04-12 10:58:53 +0200

    Add page to edit events

---
+++ aspnetcore_project/Pages/EditEvent.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using aspnetcore_project.Data;
+using aspnetcore_project.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace aspnetcore_project.Pages
+{
+    [Authorize(Roles = "Administrator,Organizer")]
+    public class EditEventModel : PageModel
+    {
+        private readonly aspnetcore_project.Data.EventDbContext _context;
+
+        public EditEventModel(aspnetcore_project.Data.EventDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Event Event { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event = await _context.Events.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Event).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventExists(Event.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./OrganizeEvents");
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Events.Any(e => e.Id == id);
+        }
+    }
+}
